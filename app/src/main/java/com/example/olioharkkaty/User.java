d065922bf65043efa0cb3c9ac32ebb98ddf18f93
Tomsongolli <tommi.tynkkynen@student.lut.fi>
2020-04-23 21:49:42 +0300

    sign in ikkuna

--- app/src/main/java/com/example/olioharkkaty/User.java
+++ app/src/main/java/com/example/olioharkkaty/User.java
@@ -1,7 +1,20 @@
 package com.example.olioharkkaty;
 
-public class User {
-    private String f_name = "Jarkko";
-    private String l_name = "Esimerkki";
-    private String adress = "Esimerkkikatu 1 A 20, 10101 Helsinki";
+public class User(String un, String pw, String fn, String ln, String ad) {
+
+    private String u_name;
+    private String password;
+    private String f_name;
+    private String l_name;
+    private String adress;
+
+    public User(String un, String pw, String fn, String ln, String ad){
+        u_name = un;
+        password = pw;
+        f_name = fn;
+        l_name = ln;
+        adress = ad;
+
+    }
+
 }
