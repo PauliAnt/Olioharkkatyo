7950358d867606adc0b10e32d9797ab821ce6429
Tomsongolli <tommi.tynkkynen@student.lut.fi>
2020-04-23 14:01:21 +0300

    kommentti lisätty

---
+++ app/src/main/java/com/example/olioharkkaty/CalenderAct.java
@@ -0,0 +1,23 @@
+package com.example.olioharkkaty;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.CalendarView;
+import android.widget.Spinner;
+
+public class CalenderAct extends AppCompatActivity {
+
+    CalendarView calendarView;
+    Spinner hallRoom;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_calender);
+
+        calendarView = (CalendarView) findViewById(R.id.calendarView)
+    }
+
+
+}
