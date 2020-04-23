5f8aa380361d399a2f01cfb26a22684bc4864375
Tomsongolli <tommi.tynkkynen@student.lut.fi>
2020-04-23 14:22:36 +0300

    Kalenteri activityn spinneri lisätty

--- app/src/main/java/com/example/olioharkkaty/CalenderActivity.java
+++ app/src/main/java/com/example/olioharkkaty/CalenderActivity.java
@@ -3,20 +3,32 @@ package com.example.olioharkkaty;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
 import android.widget.CalendarView;
 import android.widget.Spinner;
+
+import java.util.ArrayList;
 
 public class CalenderActivity extends AppCompatActivity {
 
     CalendarView calendarView;
     Spinner hallRoom;
+    ArrayList<String> rooms;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calender);
 
-        calendarView = (CalendarView) findViewById(R.id.calendarView)
+        calendarView = (CalendarView) findViewById(R.id.calendarView);
+        hallRoom = (Spinner) findViewById(R.id.spinner);
+
+        Hall hall = Hall.getInstance();
+        rooms = hall.getRooms();
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, rooms);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        hallRoom.setAdapter(adapter);
     }
 
 
